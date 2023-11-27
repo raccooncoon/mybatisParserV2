e41a2c0a9d2f6289010c8a2e54d1568937a82a19
임민영 <raccoon@shinsegae.com>
2023-11-28 01:48:06 +0900

    api 경로 추가 프록시 추가 도커 네트워크 추가

--- src/main/java/com/example/mybatisparser/XmlController.java
+++ src/main/java/com/example/mybatisparser/XmlController.java
@@ -4,15 +4,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class XmlController {
 
     final XmlService xmlService;
