0212f7242cbbac3dec67c721d0ddd5346664e250
임민영 <raccoon@shinsegae.com>
2023-12-02 13:22:07 +0900

    자기 자신 있는 경우 예외 추가

--- src/main/java/com/example/mybatisparser/JavaInfoProcess.java
+++ src/main/java/com/example/mybatisparser/JavaInfoProcess.java
@@ -17,19 +17,36 @@ public class JavaInfoProcess {
 
     private final JavaInfoRepository javaInfoRepository;
     private final NodeRepository nodeRepository;
+    private final XmlRepository xmlRepository;
 
     private static String getServiceName() {
-        return "papaya-master";
+        return "coral-release";
     }
 
     public void process() {
-        String methodCall = "mergeTaxSavePhoneNumber";
-        List<JavaInfoEntity> firstJavaInfos = javaInfoRepository.findByMethodCallsContainingAndServiceName(methodCall, getServiceName());
-        //getLog(firstJavaInfos);
 
+        List<XmlEntity> xmlEntities = xmlRepository.findByServiceNameAndMapperTypeIn(
+                getServiceName(),
+                List.of("create", "update", "delete")
+        );
+
+        xmlEntities.forEach(xmlEntity -> {
+            log.info("xmlEntity : {}", xmlEntity.getId());
+            log.info("xmlEntity : {}", xmlEntity.getMapperType());
+            log.info("xmlEntity : {}", xmlEntity.getServiceName());
+            log.info("xmlEntity : {}", xmlEntity.getMapperId());
+
+            processJavaInfo(xmlEntity.getMapperId(), xmlEntity.getServiceName());
+        });
+
+//        processJavaInfo("getRefundPossibleInfo", getServiceName());
+    }
+
+    private void processJavaInfo(String methodCall, String serviceName) {
+        List<JavaInfoEntity> firstJavaInfos = javaInfoRepository.findByMethodCallsContainingAndServiceName(methodCall, serviceName);
         firstJavaInfos.forEach(javaInfo -> {
                     JavaNodeRecord javaNodeRecord = new JavaNodeRecord(javaInfo, List.of(javaInfo.getId().toString()));
-                    extracted(javaNodeRecord);
+                    extracted(javaNodeRecord, serviceName);
                 }
         );
     }
@@ -38,10 +55,11 @@ public class JavaInfoProcess {
         nodeRepository.deleteAll();
     }
 
-    private void extracted(JavaNodeRecord javaNodeRecord) {
+    private void extracted(JavaNodeRecord javaNodeRecord, String serviceName) {
         List<JavaInfoEntity> nextJavaInfos = javaInfoRepository.findByMethodCallsContainsAndClassFieldsContainsAndServiceName(
-                javaNodeRecord.currentJavaInfoEntity().getMethodName(), javaNodeRecord.currentJavaInfoEntity().getClassName(), getServiceName());
+                javaNodeRecord.currentJavaInfoEntity().getMethodName(), javaNodeRecord.currentJavaInfoEntity().getClassName(), serviceName);
 
+        // 조회 값이 없는 경우 저장 후 완료
         if (nextJavaInfos.isEmpty()) {
 //            log.info("nextJavaInfos is empty");
             log.info("javaInfoIds : {}", javaNodeRecord.javaInfoIds());
@@ -55,18 +73,13 @@ public class JavaInfoProcess {
 
         nextJavaInfos.forEach(nextJavaInfo -> {
             List<String> allIds = new ArrayList<>(javaNodeRecord.javaInfoIds());
-            allIds.add(nextJavaInfo.getId().toString());
-            JavaNodeRecord javaNodeRecord1 = new JavaNodeRecord(nextJavaInfo, allIds);
-            extracted(javaNodeRecord1);
+            String nextInfoId = nextJavaInfo.getId().toString();
+
+            // 자기 자신이 이미 포함되어 있지 않은 경우에만 재귀 호출
+            if (!allIds.contains(nextInfoId)) {
+                allIds.add(nextInfoId);
+                extracted(new JavaNodeRecord(nextJavaInfo, allIds), serviceName);
+            }
         });
     }
-
-/*    private static void getLog(List<JavaInfoEntity> list) {
-        list.forEach(javaInfoEntity -> {
-            log.info("javaInfoEntity : {}", javaInfoEntity.getId());
-            log.info("javaInfoEntity : {}", javaInfoEntity.getClassName());
-            log.info("javaInfoEntity : {}", javaInfoEntity.getMethodName());
-        });
-    }*/
-
 }
--- src/main/java/com/example/mybatisparser/XmlRepository.java
+++ src/main/java/com/example/mybatisparser/XmlRepository.java
@@ -8,8 +8,13 @@ import java.util.List;
 
 public interface XmlRepository extends JpaRepository<XmlEntity, Long> {
 
+
+    List<XmlEntity> findByServiceNameAndMapperTypeIn(String services, List<String> mapperTypes);
+
     Page<XmlEntity> findByMapperIdContainsAndMapperTypeIn(String mapperId, List<String> mapperTypes, Pageable pageable);
+
     Page<XmlEntity> findByMapperTypeContains(String mapperId, Pageable pageable);
-//    Page<XmlEntity> findByMapperBodyContains(String mapperId, Pageable pageable);
+
+    //    Page<XmlEntity> findByMapperBodyContains(String mapperId, Pageable pageable);
     Page<XmlEntity> findByMapperBodyContainsAndMapperTypeIn(String mapperId, List<String> mapperTypes, Pageable pageable);
 }
